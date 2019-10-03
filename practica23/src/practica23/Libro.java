32c04578ef070b7ff4590438fa85cd9b252f10b1
18PROGB0128 <18PROGB0128@TS-LABS-2.labs.cbtis94.local>
2019-10-03 13:30:54 -0500

    se creo el proyecto basico, la clase libro, los costructores, los get y set.

--- practica23/src/practica23/Libro.java
+++ practica23/src/practica23/Libro.java
@@ -5,5 +5,54 @@ package practica23;
  * @author 18PROGB0128
  */
 public class Libro {
+    //creamos los atributos
+    private String titulo;
+    private String autor;
+    private int numeroejemplaresexistentes;
+    private int numeroejemplaresprestados;
+    
+    //creamos los constructores
+    public Libro() {
+    }
 
+    public Libro(String titulo, String autor, int numeroejemplaresexistentes, int numeroejemplaresprestados) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroejemplaresexistentes = numeroejemplaresexistentes;
+        this.numeroejemplaresprestados = numeroejemplaresprestados;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroejemplaresexistentes() {
+        return numeroejemplaresexistentes;
+    }
+
+    public void setNumeroejemplaresexistentes(int numeroejemplaresexistentes) {
+        this.numeroejemplaresexistentes = numeroejemplaresexistentes;
+    }
+
+    public int getNumeroejemplaresprestados() {
+        return numeroejemplaresprestados;
+    }
+
+    public void setNumeroejemplaresprestados(int numeroejemplaresprestados) {
+        this.numeroejemplaresprestados = numeroejemplaresprestados;
+    }
+    
+    
 }
