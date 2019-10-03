ff0ea8364a1412db86e9c561907aae1f5447a051
18PROGB0128 <18PROGB0128@TS-LABS-2.labs.cbtis94.local>
2019-10-03 13:32:50 -0500

    se creo el proyecto basico, la clase libro, los costructores, los get y set.

--- practica23/src/practica23/Libro.java
+++ practica23/src/practica23/Libro.java
@@ -22,6 +22,7 @@ public class Libro {
         this.numeroejemplaresprestados = numeroejemplaresprestados;
     }
 
+    //creamos los get y set
     public String getTitulo() {
         return titulo;
     }
